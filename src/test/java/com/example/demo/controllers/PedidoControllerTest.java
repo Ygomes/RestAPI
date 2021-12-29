da9eb1cd57331eac47bb167ceb9c9d9a92a20f18
yuri.oliveira <yuri.oliveira@me.com.br>
2021-12-29 09:50:39 -0300

    testes

--- src/test/java/com/example/demo/controllers/PedidoControllerTest.java
+++ src/test/java/com/example/demo/controllers/PedidoControllerTest.java
@@ -1,15 +1,12 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entity.Status;
-import com.example.demo.service.PedidoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -36,6 +33,7 @@ class PedidoControllerTest {
 
     String pedido = "123456";
     Status getNewStatus = new Status("APROVADO", 3, 20, pedido);
+
 
     @Test
     void itShouldsetNewStatusInvalido() throws Exception {
